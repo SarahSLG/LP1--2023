87878312a16902bc5dfd6063669257e85093ed71
SarahSLG <126773265+SarahSLG@users.noreply.github.com>
2023-05-31 20:55:27 -0300

    Atualizar o Biblioteca.java

--- 2°Bimestre/Biblioteca/Biblioteca/src/Biblioteca.java
+++ 2°Bimestre/Biblioteca/Biblioteca/src/Biblioteca.java
@@ -1,47 +1,70 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Biblioteca {
-    private String nome;
-    private String dono;
-    private List<Biblioteca> livros = new ArrayList();
-    private List<Biblioteca>funcionarios = new ArrayList();
-
-    public Biblioteca(String nome, String dono) {
-        this.nome = nome;
-        this.dono = dono;
-    }
-
-    //set e get
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDono() {
-        return dono;
-    }
-
-    public List<Biblioteca> getLivros() {
-        return livros;
-    }
-
-    public List<Biblioteca> getFuncionarios() {
-        return funcionarios;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setDono(String dono) {
-        this.dono = dono;
-    }
-
-    public void setLivros(List<Biblioteca> livros) {
-        this.livros = livros;
-    }
-
-    public void setFuncionarios(List<Biblioteca> funcionarios) {
-        this.funcionarios = funcionarios;
-    }
-}
+import java.util.ArrayList; 
+ import java.util.List; 
+  
+ public class biblioteca { 
+     private String nome; 
+     private Dono dono; 
+     private List<livros> Livros = new ArrayList<>(); 
+     private List<funcionarios> Func = new ArrayList<>(); 
+  
+     public biblioteca(String nome, Dono dono) { 
+         this.nome = nome; 
+         this.dono = dono; 
+     } 
+  
+     public List<livros> getLivros() { 
+         return Livros; 
+     } 
+  
+     public void setLivros(List<livros> livros) { 
+         this.Livros = Livros; 
+     } 
+  
+     public void addlivros(livros livro){ 
+         Livros.add(livro); 
+     } 
+  
+     public List<funcionarios> getFunc() { 
+         return Func; 
+     } 
+  
+     public void setFunc(List<funcionarios> funcionarios) { 
+         this.Func = Func; 
+     } 
+  
+     public void addfuncionarios(funcionarios Funcionario){ 
+         Func.add(Funcionario); 
+     } 
+  
+     public String getNome() { 
+         return nome; 
+     } 
+  
+     public void setNome(String nome) { 
+         this.nome = nome; 
+     } 
+  
+     public Dono getDono() { 
+         return dono; 
+     } 
+  
+     public void setDono(Dono dono) { 
+         this.dono = dono; 
+     } 
+  
+     public void mostrarLivros(){ 
+         for (int i = 0; i < Livros.size(); i++) { 
+             System.out.println(Livros.get(i).getNome()); 
+             System.out.println(Livros.get(i).getAutor()); 
+             System.out.println(Livros.get(i).getAno()); 
+         } 
+     } 
+  
+     public void mostrarFuncionarios(){ 
+         for (int i = 0; i < Func.size(); i++) { 
+             System.out.println(Func.get(i).getNome()); 
+             System.out.println(Func.get(i).getCPF()); 
+         } 
+     } 
+  
+ }
