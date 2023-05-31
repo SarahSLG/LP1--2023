26456734900d6ab5732d9c03d37584d2481f25c6
SarahSLG <126773265+SarahSLG@users.noreply.github.com>
2023-05-31 20:57:02 -0300

    Atualizar o Livros.java

--- 2°Bimestre/Biblioteca/Biblioteca/src/Livros.java
+++ 2°Bimestre/Biblioteca/Biblioteca/src/Livros.java
@@ -1,39 +1,35 @@
-public class Livros {
-    private String nome;
-    private String autor;
-    private int ano;
-
-    //construtores
-
-
-    public Livros(String nome, String autor, int ano) {
-        this.nome = nome;
-        this.autor = autor;
-        this.ano = ano;
-    }
-
-    //set e get
-    public String getNome() {
-        return nome;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-}
+public class livros { 
+     private String nome; 
+     private String autor; 
+     private int ano; 
+  
+     public livros(String nome, String autor, int ano) { 
+         this.nome = nome; 
+         this.autor = autor; 
+         this.ano = ano; 
+     } 
+  
+     public String getNome() { 
+         return nome; 
+     } 
+  
+     public void setNome(String nome) { 
+         this.nome = nome; 
+     } 
+  
+     public String getAutor() { 
+         return autor; 
+     } 
+  
+     public void setAutor(String autor) { 
+         this.autor = autor; 
+     } 
+  
+     public int getAno() { 
+         return ano; 
+     } 
+  
+     public void setAno(int ano) { 
+         this.ano = ano; 
+     } 
+ }
